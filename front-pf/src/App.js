4c368c0e1ad8d60d0429dd3863df0e87d9876016
sebastian <sebastyandx@hotmail.com>
2022-09-29 22:04:18 -0300

    PageExcursiones

--- front-pf/src/App.js
+++ front-pf/src/App.js
@@ -2,20 +2,22 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Register from "./pages/Register"
-import CreateCard from "./pages/CreateCard"
-import Destinations from './pages/Destinations/Destinations';
-import "bootstrap/dist/css/bootstrap.min.css"
+import Register from "./pages/Register";
+import CreateCard from "./pages/CreateCard";
+import Destinations from "./pages/Destinations/Destinations";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Excursiones from "./pages/Excursiones/Excursiones";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/cards/create" element={<CreateCard />}/>
-        <Route path="/destinations" element={<Destinations/>}></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/cards/create" element={<CreateCard />} />
+        <Route path="/destinations" element={<Destinations />}></Route>
+        <Route path="/excursiones" element={<Excursiones />} />
       </Routes>
     </div>
   );
