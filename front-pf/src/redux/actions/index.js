3861d90aefcad2225530d0377c0717e6dc13e841
Tobi Tortosa <tobiastortosa17@gmail.com>
2022-10-01 18:45:02 -0300

    added pack details page

--- front-pf/src/redux/actions/index.js
+++ front-pf/src/redux/actions/index.js
@@ -1,8 +1,8 @@
-
 import {
   GET_ALL_EXCURSION,
   GET_ALL_PACKS,
   GET_ALL_HOTEL,
+  GET_PACK_BY_ID,
 } from "./actionsTypes";
 import { CREATE_USER, GET_CURRENT_USER } from "./actionsTypes";
 
@@ -15,7 +15,6 @@ export function getHotels() {
       .catch((error) => console.log(error));
   };
 }
-
 
 export function getExcursiones() {
   return (dispatch) => {
@@ -31,6 +30,10 @@ export function getExcursiones() {
 // };
 
 export const getCurrentUser = (obj) => (dispatch) => {
-  dispatch({ type: GET_CURRENT_USER, payload: obj });
+  return dispatch({ type: GET_CURRENT_USER, payload: obj });
 };
 
+export const getPackById = (id) => async (dispatch) => {
+  let pack = await axios.get(`http://localhost:5000/packs/${id}`);
+  return dispatch({ type: GET_PACK_BY_ID, payload: pack });
+};
