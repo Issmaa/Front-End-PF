6ed30a36bb80c5d56f777bb930115f58a9c79c56
sebastian <sebastyandx@hotmail.com>
2022-10-04 12:52:19 -0300

    cardHoteles

--- front-pf/src/App.js
+++ front-pf/src/App.js
@@ -2,26 +2,27 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Register from "./pages/Register"
-import AboutUs from './pages/AboutUs/AboutUs.jsx';
+import Register from "./pages/Register";
+import AboutUs from "./pages/AboutUs/AboutUs.jsx";
 import Excursiones from "./pages/Excursiones/Excursiones";
-import "bootstrap/dist/css/bootstrap.min.css"
-import Destinations from './pages/Destinations/Destinations';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Destinations from "./pages/Destinations/Destinations";
 import PageUser from "./pages/PageUser/PageUser";
 import InfoCard from "./pages/InfoCard/InfoCard";
-
+import PageHotel from "./pages/Packs/Hotel";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/destinations" element={<Destinations/>} />
-        <Route path="/aboutus" element={<AboutUs />}/>
-        <Route path="/excursiones" element={<Excursiones/>}/>
-        <Route path="/detail/:id" element={<InfoCard/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/excursiones" element={<Excursiones />} />
+        <Route path="/detail/:id" element={<InfoCard />} />
         <Route path="/user" element={<PageUser />} />
+        <Route path="/page-hotel" element={<PageHotel />} />
       </Routes>
     </div>
   );
